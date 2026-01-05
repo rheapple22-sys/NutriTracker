718d44fbf53686d99028f08545112d270fa137be
Rhea <rheapple22@gmail.com>
2026-01-05 21:26:07 +0530

    added console to check the client id

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,11 @@ import App from "./App";
 import { AuthProvider } from "./auth/AuthProvider";
 import "./index.css";
 
+console.log(
+  "VITE_GOOGLE_CLIENT_ID from env:",
+  import.meta.env.VITE_GOOGLE_CLIENT_ID
+);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
